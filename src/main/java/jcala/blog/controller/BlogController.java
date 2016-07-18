5f316a425c27a2011970ce17ce8ca2afae69f8a7
jcala <jcalaz@163.com>
2016-07-18 11:29:58 +0800

    修改tags为foreach

--- src/main/java/jcala/blog/controller/BlogController.java
+++ src/main/java/jcala/blog/controller/BlogController.java
@@ -1,7 +1,13 @@
 package jcala.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import jcala.blog.entity.Tag;
+
 
 /**
  * Created by jcala on 2016/7/14
@@ -29,7 +35,13 @@ public class BlogController {
     }
 
     @GetMapping("/tags")
-    public String tags() {
+    public String tags(Model model) {
+        List<Tag> tags=new ArrayList<>();
+        tags.add(new Tag("home","/#home"));
+        tags.add(new Tag("blogs","/blogs"));
+        tags.add(new Tag("projects","/projects"));
+        tags.add(new Tag("about","/about"));
+        model.addAttribute("tags",tags);
         return "tags";
     }
 }
---
+++ src/main/java/jcala/blog/entity/Tag.java
@@ -0,0 +1,33 @@
+package jcala.blog.entity;
+
+/**
+ * Created by jcala on 2016/7/18
+ */
+public class Tag {
+    private String title;
+    private String href;
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Tag(String title, String href) {
+        this.title = title;
+        this.href = href;
+    }
+
+    public Tag() {
+    }
+}
