f91b54f685a75a8e2962d301e6032693d1289a6b
jcala <jcalaz@163.com>
2016-07-20 14:56:01 +0800

    完成了Project视图的绑定

--- src/main/java/jcala/blog/controller/BlogController.java
+++ src/main/java/jcala/blog/controller/BlogController.java
@@ -1,17 +1,14 @@
 package jcala.blog.controller;
 
+import jcala.blog.entity.Project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import jcala.blog.entity.Tag;
-
-
-/**
- * Created by jcala on 2016/7/14
- */
 @Controller
 public class BlogController {
     @GetMapping("/blogs")
@@ -37,6 +34,18 @@ public class BlogController {
     public String projects(Model model) {
         model.addAttribute("headerH1","projects");
         model.addAttribute("headerP","我的博客列表");
+        List<Project> projects=new ArrayList<>();
+        Project project=new Project();
+        project.setName("JcalaBlog");
+        project.setShowHref("https://jcala.me/projects#jcalaBlog");
+        project.setOpenSourceHref("https://github.com/jcalaz/jcalaBlog");
+        project.setProjectDate(new Date());
+        project.setSummary("SpringBlog is a very simple and clean-design blog system implemented with " +
+                "Spring Boot. It is one of my learning projects to explore awesome features in Spring " +
+                "Boot web programming. It is also the source code of my blog site ");
+        project.setTechnology("Spring Boot/MVC/JPA + Hibernate + MySQL + Redis + Bootstrap + Jade");
+        projects.add(project);
+        model.addAttribute("projects",projects);
         return "projects";
     }
 
---
+++ src/main/java/jcala/blog/entity/Project.java
@@ -0,0 +1,76 @@
+package jcala.blog.entity;
+
+import java.util.Date;
+
+/**
+ * Created by jcala on 2016/7/20
+ */
+public class Project {
+    private String Name;
+    private String ShowHref;
+    private String OpenSourceHref;
+    private Date ProjectDate;
+    private String Summary;
+    private String Technology;
+
+    public Project(String name, String showHref, String openSourceHref,
+                   Date projectDate, String summary, String technology) {
+        Name = name;
+        ShowHref = showHref;
+        OpenSourceHref = openSourceHref;
+        ProjectDate = projectDate;
+        Summary = summary;
+        Technology = technology;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setShowHref(String showHref) {
+        ShowHref = showHref;
+    }
+
+    public void setOpenSourceHref(String openSourceHref) {
+        OpenSourceHref = openSourceHref;
+    }
+
+    public void setProjectDate(Date projectDate) {
+        ProjectDate = projectDate;
+    }
+
+    public void setSummary(String summary) {
+        Summary = summary;
+    }
+
+    public void setTechnology(String technology) {
+        Technology = technology;
+    }
+
+    public Project() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getShowHref() {
+        return ShowHref;
+    }
+
+    public String getOpenSourceHref() {
+        return OpenSourceHref;
+    }
+
+    public Date getProjectDate() {
+        return ProjectDate;
+    }
+
+    public String getSummary() {
+        return Summary;
+    }
+
+    public String getTechnology() {
+        return Technology;
+    }
+}
