56b70d3922aae8f634c870a2a5dff5063f481873
jcala <jcalaz@163.com>
2016-07-23 14:44:42 +0800

    删除了无关的bean

--- src/main/java/jcala/blog/Application.java
+++ src/main/java/jcala/blog/Application.java
@@ -15,35 +15,4 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
-	/*@Bean(name = "hikariDataSource")
-	public DataSource hikariDataSource() {
-		HikariDataSource hds = new HikariDataSource();
-		hds.setUsername("root");
-		hds.setPassword("10573962Zj");
-		hds.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/jcala_blog?useUnicode=true&amp;characterEncoding=UTF-8");
-		hds.setDriverClassName("com.mysql.jdbc.Driver");
-		hds.setConnectionTimeout(30000);
-		hds.setMaximumPoolSize(4);
-		hds.setMinimumIdle(20);
-		return hds;
-	}
-	@Bean(name = "sqlSessionFactory")
-	@Autowired
-	@Qualifier("hikariDataSource")
-	public SqlSessionFactory sqlSessionFactory(DataSource hikariDataSource) throws Exception {
-		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-		bean.setDataSource(hikariDataSource);
-		bean.setTransactionFactory(new SpringManagedTransactionFactory());
-		org.apache.ibatis.session.Configuration config = new org.apache.ibatis.session.Configuration();
-		config.addMappers("jcala.blog.mapping");
-		bean.setConfiguration(config);
-		return bean.getObject();
-	}
-
-	@Bean(name = "sqlSessionTemplate")
-	@Autowired
-	@Qualifier("sqlSessionFactory")
-	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-		return new SqlSessionTemplate(sqlSessionFactory);
-	}*/
 }
