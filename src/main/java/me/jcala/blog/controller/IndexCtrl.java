fa29da937d7dff1ae0066a14e45e1d159f726605
jcala <jcalaz@163.com>
2016-08-06 17:33:35 +0800

    velocity界面采用include模块化

--- src/main/java/me/jcala/blog/controller/IndexCtrl.java
+++ src/main/java/me/jcala/blog/controller/IndexCtrl.java
@@ -1,22 +1,15 @@
 package me.jcala.blog.controller;
 
-import me.jcala.blog.service.IndexSer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Map;
 
 /**
  * Created by jcala on 2016/7/10
  */
 @Controller
 public class IndexCtrl {
-    @Autowired
-    IndexSer indexSer;
     @GetMapping("/")
-    public String welcome(Map<String, Object> model) {
-        model.put("username", indexSer.getUserName());
+    public String welcome() {
         return "index";
     }
 }
