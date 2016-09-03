5bcb4700a350062a331d678af284b961b368df94
jcala <jcalaz@163.com>
2016-09-03 15:29:49 +0800

    添加了BlogView和BlogTag多对多映射关系的ViewTag bean

---
+++ src/main/java/me/jcala/blog/domain/ViewTag.java
@@ -0,0 +1,18 @@
+package me.jcala.blog.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by Administrator on 2016/9/3.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ViewTag {
+    private Integer vid;
+    private Integer tid;
+}
