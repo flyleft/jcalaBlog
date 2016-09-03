ef0ee6bc87234ab05cf3ac5d49a5eaa543d9bdae
jcala <jcalaz@163.com>
2016-09-04 00:07:26 +0800

    增加了存储博客是否成功的提示框

--- src/main/java/me/jcala/blog/controller/admin/AdminCtrl.java
+++ src/main/java/me/jcala/blog/controller/admin/AdminCtrl.java
@@ -3,10 +3,13 @@ package me.jcala.blog.controller.admin;
 import me.jcala.blog.service.AdminBlogSer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.SQLException;
 
@@ -23,17 +26,30 @@ public class AdminCtrl {
         return "admin/moniter";
     }
     @GetMapping("/newBlog")
-    public String newBlog() {
+    public String newBlogBefore() {
+        return "admin/newBlog";
+    }
+    @GetMapping("/newBlog/{id}")
+    public String newBlogAfter(@PathVariable("id") String id,Model model) {
+       if ("1".equals(id)){
+          model.addAttribute("myId",1);
+       }
+       else{
+           model.addAttribute("myId",2);
+       }
         return "admin/newBlog";
     }
     @PostMapping("/saveBlog")
     public ModelAndView saveBlog(String title,String tags,String article) {
+        int id;
         try {
             adminBlogSer.addBlog(title,tags,article);
+            id=1;
         } catch (Exception e) {
+            id=2;
            System.out.println("============="+e.getMessage());
         }
-        return new ModelAndView("redirect:/admin/newBlog");
+        return new ModelAndView("redirect:/admin/newBlog/"+id);
     }
     @GetMapping("/blogSet")
     public String blogSet() {
