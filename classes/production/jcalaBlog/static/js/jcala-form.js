7d068e35b16cf53fa37a96ba39c131a7b71de358
jcala <jcalaz@163.com>
2016-09-15 17:14:02 +0800

    给blog_modify页面增加了markdown主题;并将js从页面中分离

--- classes/production/jcalaBlog/static/js/jcala-form.js
+++ classes/production/jcalaBlog/static/js/jcala-form.js
@@ -7,4 +7,12 @@ $(function() {
         document.blogForm.md.value=testEditor.getMarkdown();
         form.submit();
     });
+    $("#modify-blog").bind('click', function(){
+        var form = document.forms[0];
+        form.action = "/admin/update/${blog.Vid}";
+        form.method = "post";
+        document.blogForm.article.value=testEditor.getHTML();
+        document.blogForm.md.value=testEditor.getMarkdown();
+        form.submit();
+    });
 });
