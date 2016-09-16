27369a827bcabdf84224001e978f1ac96a4d99a3
jcala <jcalaz@163.com>
2016-09-16 18:30:13 +0800

    修改了about页面的显示;清除了原来的UserSer和TempSer

--- src/main/java/me/jcala/blog/controller/FontEndCtrl.java
+++ src/main/java/me/jcala/blog/controller/FontEndCtrl.java
@@ -1,8 +1,6 @@
 package me.jcala.blog.controller;
 
 import me.jcala.blog.service.BlogSer;
-import me.jcala.blog.service.TempSer;
-import me.jcala.blog.service.UserSer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,20 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FontEndCtrl {
     @Autowired
-    private UserSer userSer;
-    @Autowired
-    private TempSer tempSer;
-    @Autowired
     private BlogSer blogSer;
     @GetMapping("/archives")
     public String archives(Model model){
-        model.addAttribute("archivesYears",tempSer.archives());
         return "archives";
     }
 
     @GetMapping("/projects")
     public String projects(Model model) {
-        model.addAttribute("projects",tempSer.projects());
         return "projects";
     }
 
@@ -35,7 +27,6 @@ public class FontEndCtrl {
     }
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("userInfo",userSer.getUserInfo());
         return "about";
     }
     @GetMapping("/post")
