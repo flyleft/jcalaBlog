5043dc62190e860c67c11f97c7545a153af953f7
jcala <jcalaz@163.com>
2016-09-20 16:07:11 +0800

    将admin/project页面js分离

--- classes/production/jcalaBlog/static/js/jcala-form.js
+++ classes/production/jcalaBlog/static/js/jcala-form.js
@@ -23,4 +23,46 @@ $(function() {
         document.resumeForm.md.value=testEditor.getMarkdown();
         form.submit();
     });
+    $(document).on("click", "#add_project", function (e) {
+        bootbox.dialog({
+                title: "New Project",
+                message: '<div class="row">  ' +
+                '<div class="col-md-12"> ' +
+                '<form class="form-horizontal" id="add_pro_form"> ' +
+                '<div class="form-group"> ' +
+                '<label class="col-md-2 control-label">Name</label> ' +
+                '<div class="col-md-9"> ' +
+                '<input name="name" type="text" placeholder="project name" class="form-control input-md"> ' +
+                '</div></div>' +
+                '<div class="form-group"> ' +
+                '<label class="col-md-2 control-label">Addr</label> ' +
+                '<div class="col-md-9"> ' +
+                '<input name="url" type="text" placeholder="project url" class="form-control input-md"> ' +
+                '</div></div>' +
+                '<div class="form-group"> ' +
+                '<label class="col-md-2 control-label">Tech</label> ' +
+                '<div class="col-md-9"> ' +
+                '<textarea class="form-control" rows="3" name="tech" maxlength="220"></textarea> ' +
+                '</div></div>' +
+                '<div class="form-group"> ' +
+                '<label class="col-md-2 control-label">Desc</label> ' +
+                '<div class="col-md-9"> ' +
+                '<textarea class="form-control" rows="5" name="desp"></textarea> ' +
+                '</div></div>' +
+                '</form> </div>  </div>',
+                buttons: {
+                    success: {
+                        label: "Save",
+                        className: "btn-success",
+                        callback: function () {
+                            var form =document.getElementById("add_pro_form");
+                            form.action = "/admin/addPro.action";
+                            form.method = "post";
+                            form.submit();
+                        }
+                    }
+                }
+            }
+        );
+    });
 });
