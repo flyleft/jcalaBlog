3937000472340488dda46767f2421c18104f7132
jcala <jcalaz@163.com>
2016-09-20 23:36:58 +0800

    更改了BlogMapper的方法名

--- src/main/java/me/jcala/blog/mapping/BlogMapper.java
+++ src/main/java/me/jcala/blog/mapping/BlogMapper.java
@@ -14,41 +14,6 @@ import java.util.List;
 @Repository
 @Mapper
 public interface BlogMapper {
-      @Insert({"insert into blog_view " ,
-              "(date,title,article,tags,md) " ,
-              "values(#{bv.date},#{bv.title}," ,
-              "#{bv.article},#{bv.tags},#{bv.md})"})
-      @SelectKey(before=false,keyProperty="bv.vid",resultType=Integer.class,
-              statementType= StatementType.STATEMENT,statement="SELECT LAST_INSERT_ID() AS id")
-      int insertBlog(@Param("bv") BlogView blogView) throws Exception;
-
-      @Insert("insert ignore into view_tag (name,vid) values(#{tn},#{id})")
-      int insertViewTag(@Param("tn") String tagName,@Param("id") int vid) throws Exception;
-
-      @Delete("delete from view_tag where vid = #{vid}")
-      int deleteViewTag(@Param("vid") int vid) throws Exception;
-
-      @Delete("delete from blog_view where vid =#{vid} limit 1")
-      int deleteBlogView(@Param("vid") int vid) throws Exception;
-
-      @Select({
-            "select title,tags,md",
-              "from blog_view",
-              "where vid = #{id}",
-              "limit 1"
-      })
-      BlogView selectAdmin(@Param("id") int id) throws Exception;
-
-      @Update({
-            "update blog_view",
-              "set title = #{bv.title},",
-              "tags = #{bv.tags},",
-              "md = #{bv.md},",
-              "article = #{bv.article}",
-              "where vid = #{bv.vid}"
-      })
-      void updateBlogView(@Param("bv") BlogView blogView) throws Exception;
-
       @Select({
               "select vid,title,tags",
               "from blog_view",
@@ -68,4 +33,40 @@ public interface BlogMapper {
               "order by date desc",
               "limit #{st},12"})
       List<BlogView> selectArc(@Param("st") int start) throws Exception;
+
+      @Select({
+              "select title,tags,md",
+              "from blog_view",
+              "where vid = #{id}",
+              "limit 1"
+      })
+      BlogView selectAdmin(@Param("id") int id) throws Exception;
+
+      @Insert({"insert into blog_view " ,
+              "(date,title,article,tags,md) " ,
+              "values(#{bv.date},#{bv.title}," ,
+              "#{bv.article},#{bv.tags},#{bv.md})"})
+      @SelectKey(before=false,keyProperty="bv.vid",resultType=Integer.class,
+              statementType= StatementType.STATEMENT,statement="SELECT LAST_INSERT_ID() AS id")
+      int insertBlog(@Param("bv") BlogView blogView) throws Exception;
+
+      @Insert("insert ignore into view_tag (name,vid) values(#{tn},#{id})")
+      int insertViewTag(@Param("tn") String tagName,@Param("id") int vid) throws Exception;
+
+      @Delete("delete from view_tag where vid = #{vid}")
+      int deleteViewTag(@Param("vid") int vid) throws Exception;
+
+      @Delete("delete from blog_view where vid =#{vid} limit 1")
+      int deleteBlogView(@Param("vid") int vid) throws Exception;
+
+      @Update({
+            "update blog_view",
+              "set title = #{bv.title},",
+              "tags = #{bv.tags},",
+              "md = #{bv.md},",
+              "article = #{bv.article}",
+              "where vid = #{bv.vid}"
+      })
+      void updateBlogView(@Param("bv") BlogView blogView) throws Exception;
+
 }
