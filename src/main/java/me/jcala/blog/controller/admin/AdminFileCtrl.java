cac8804b7113fde047655dbf7d217f24107f8cf7
jcala <jcalaz@163.com>
2016-09-25 02:27:45 +0800

    实现了图片的正确上传，但返回了错误的本地链接

--- src/main/java/me/jcala/blog/controller/admin/AdminFileCtrl.java
+++ src/main/java/me/jcala/blog/controller/admin/AdminFileCtrl.java
@@ -1,11 +1,10 @@
 package me.jcala.blog.controller.admin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import me.jcala.blog.domain.PicUpload;
+import me.jcala.blog.domain.UploadPic;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -24,7 +23,7 @@ public class AdminFileCtrl {
     private static final String PICDIR=System.getProperties().getProperty("user.home")+
             File.separatorChar+"blog"+File.separatorChar+"pic";
     @PostMapping("/admin/file/uplPic.action")
-    public String uploadPic(HttpServletRequest request) throws Exception{
+    public void uploadPic(HttpServletRequest request,HttpServletResponse response) throws Exception{
         MultipartHttpServletRequest multipartRequest =
                 (MultipartHttpServletRequest) request;
         Iterator<String> fileNames = multipartRequest.getFileNames();
@@ -33,6 +32,7 @@ public class AdminFileCtrl {
         String fileName = multipartFile.getOriginalFilename();
         File path=new File(PICDIR);
         File targetFile = new File(PICDIR+File.separatorChar+fileName);
+        boolean result=true;
         try {
             if (!path.exists()){
                 path.mkdirs();
@@ -42,43 +42,28 @@ public class AdminFileCtrl {
             }
         } catch (Exception e) {
             e.printStackTrace();
+            result=false;
         }
-        System.out.println("文件名:"+targetFile.getAbsolutePath());
-        PicUpload upload=new PicUpload();
-        upload.setSuccess(1);
-        upload.setMessage("Upload picture success!");
-        upload.setUrl(PICDIR+File.separatorChar+fileName);
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(upload);
+        UploadPic upload = new UploadPic();
+        if (result) {
+            upload.setSuccess(1);
+            upload.setMessage("Upload picture success!");
+            upload.setUrl(PICDIR + File.separatorChar + fileName);
+        }else {
+            upload.setSuccess(0);
+            upload.setMessage("Upload picture fail!");
+            upload.setUrl(PICDIR + File.separatorChar + fileName);
+        }
+         ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(upload));
     }
-    /**
-     * System.out.println("收到图片上传请求!");
-     String fileName = file.getOriginalFilename();
-
-     File targetFile = new File(PICDIR+File.separatorChar+fileName);
-     boolean result=true;
-     try {
-     if(!targetFile.exists()){
-     targetFile.createNewFile();
-     }
-     file.transferTo(targetFile);
-     } catch (Exception e) {
-     result=false;
-     e.printStackTrace();
-     }
-     PicUpload upload=new PicUpload();
-     if (result){
-     upload.setSuccess(1);
-     upload.setMessage("Upload picture success!");
-     upload.setUrl(PICDIR+File.separatorChar+fileName);
-     System.out.println("图片上传成功:"+targetFile.getAbsolutePath());
-     System.out.println("图片上传成功1:"+PICDIR+File.separatorChar+fileName);
-     }else {
-     upload.setSuccess(0);
-     upload.setMessage("Upload picture fail!");
-     upload.setUrl("");
-     System.out.println("图片上传失败:"+targetFile.getAbsolutePath());
-     }
-     return upload;
-     */
+   /* @GetMapping("/test")
+    public  void testJson(HttpServletResponse response) throws Exception{
+        UploadPic project=new UploadPic();
+        project.setUrl("http");
+//        project.setMessage("zhansgan");
+//        project.setSuccess(1);
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(project));
+    }*/
 }
--- src/main/java/me/jcala/blog/domain/UploadPic.java
+++ src/main/java/me/jcala/blog/domain/UploadPic.java
@@ -1,16 +1,15 @@
 package me.jcala.blog.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * Created by Administrator on 2016/9/24.
  */
 @Setter
+@Getter
 @NoArgsConstructor
-@AllArgsConstructor
-public class PicUpload {
+@ToString
+public class UploadPic {
     private int success;//0 表示上传失败，1 表示上传成功
     private String message;//图片上传提示信息,包括上传成功或上传失败及错误信息等
     private String url;//图片上传成功后返回的地址
