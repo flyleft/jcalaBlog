c0c2bebabf936f7d30970e3e824723b17abaf7f1
jcala <jcalaz@163.com>
2016-09-24 15:07:59 +0800

    添加了localStorage保存应用状态,完成黑夜模式的切换

--- src/main/resources/static/js/front-end.js
+++ src/main/resources/static/js/front-end.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
     var pos = $("#blog_content").offset().top;
     $("html,body").animate({scrollTop: pos}, 1000);
+    switch_theme();
 });
+function switch_theme() {
+    if(localStorage.blog_theme==undefined){
+        localStorage.blog_theme="light";
+    }
+    if(localStorage.blog_theme=="dark"){
+        var btn=document.getElementById("day-night");
+        var themeFont=document.getElementById("day-night-name");
+        var css=document.getElementById("theme-css");
+        var md=document.getElementById("md-container");
+        btn.className="fa fa-sun-o";
+        themeFont.textContent="DAY";
+        css.href="/css/theme-dark.css";
+        if(md!=null){
+            md.className="editormd-preview editormd-preview-theme-dark";
+        }
+    }
+}
 function day_night() {
     var btn=document.getElementById("day-night");
     var md=document.getElementById("md-container");
@@ -14,6 +32,7 @@ function day_night() {
         if(md!=null){
             md.className="editormd-preview";
         }
+        localStorage.blog_theme="light";
     }else {
         btn.className="fa fa-sun-o";
         themeFont.textContent="DAY";
@@ -21,5 +40,6 @@ function day_night() {
         if(md!=null){
             md.className="editormd-preview editormd-preview-theme-dark";
         }
+        localStorage.blog_theme="dark";
     }
 }
