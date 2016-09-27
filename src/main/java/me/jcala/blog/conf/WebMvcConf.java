07216c1ea66ff86aef6e286e8eedfa877dcce21c
jcala <jcalaz@163.com>
2016-09-27 18:37:57 +0800

    添加了从配置文件获取反向代理服务器配置的方法

--- src/main/java/me/jcala/blog/conf/WebMvcConf.java
+++ src/main/java/me/jcala/blog/conf/WebMvcConf.java
@@ -2,6 +2,7 @@ package me.jcala.blog.conf;
 
 import me.jcala.blog.interceptor.UserSecurityInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,6 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class WebMvcConf extends WebMvcConfigurerAdapter{
+    @Value("${reverse.domain}")
+    private String reverse_domain;
+    @Value("${reverse.port}")
+    private String reverse_port;
+    @Value("${reverse.prefix}")
+    private String reverse_prefix;
+    @Value("${reverse.path}")
+    private String reverse_path;
     @Autowired
     private UserSecurityInterceptor securityInterceptor;
     //@Autowired
