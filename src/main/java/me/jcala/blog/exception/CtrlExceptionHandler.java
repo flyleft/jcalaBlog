665b1ed1ab5cf196e390771a3191466f186bc61a
jcala <jcalaz@163.com>
2017-01-20 11:43:21 +0800

    将日志由logback升级为性能更高的log4j2

--- src/main/java/me/jcala/blog/exception/CtrlExceptionHandler.java
+++ src/main/java/me/jcala/blog/exception/CtrlExceptionHandler.java
@@ -2,6 +2,7 @@ package me.jcala.blog.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,9 +17,10 @@ public class CtrlExceptionHandler {
 
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public String handleIOException(Exception e) {
+    public String handleIOException(Exception e, Model model) {
         if (e!=null){
             log.warn(e.getMessage());
+            model.addAttribute("errorMsg",e.getLocalizedMessage());
         }
         return "/error";
     }
